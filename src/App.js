e56e47f826a7ab3bad17544f938a1702170378e0
Brian <bstanton773@gmail.com>
2021-11-10 17:10:07 -0600

    Add register component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import { Routes, Route } from "react-router-dom";
 import Home from './views/Home';
+import Register from './views/Register';
 
 
 export default class App extends Component {
+  register = (e) => {
+    e.preventDefault();
+    console.log(e);
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPass = e.target.confirmPass.value;
+    console.log(email, password, confirmPass)
+  }
   render() {
     return (
       <div>
@@ -12,6 +21,7 @@ export default class App extends Component {
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Register register={this.register}/>} />
           </Routes>
         </div>
 

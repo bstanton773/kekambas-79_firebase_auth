70ac47f65acaf6a81d7fd8ca959aa9f75f94bbff
Brian <bstanton773@gmail.com>
2022-02-10 17:21:35 -0600

    Change theme:

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,9 +5,9 @@ export default class Home extends Component {
         return (
             <div className="card mb-3">
                 { this.props.user ? <h2 className='text-center'>Hello {this.props.user.email}</h2>: null}
-                <img src="https://images.unsplash.com/photo-1477414348463-c0eb7f1359b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Thanksgiving" />
+                <img src="https://images.unsplash.com/photo-1644291833042-1361b57de761?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" className="card-img-top" alt="Super Bowl" />
                 <div className="card-body">
-                    <h3 className="card-title text-center">Thanksgiving Firebase Project</h3>
+                    <h3 className="card-title text-center">Super Bowl Firebase Project</h3>
                     <p className="card-text">Today we will set up Authentication with Google Firebase. You can find the documentation <a href='https://firebase.google.com/docs/auth/web/start?authuser=0'>here</a></p>
                 </div>
             </div>
